97f508e40af5e9052e7102d744c0afc17627ce51
PenDong <50320500+PenDong@users.noreply.github.com>
2020-05-18 10:48:47 +0800

    Add files via upload

---
+++ php_web/get_numbers.php
@@ -0,0 +1,16 @@
+<form action="generate_numbers.php" method="post">
+		<fieldset>
+			<legend>购买序列号</legend>
+			<ul>
+				<li>
+					<label>使用人数:</label>
+					<input type="text" name="client_numbers_of_user">
+				</li>
+				
+				<li>
+					<label> </label>
+					<input type="submit" name="sure" value="购买">
+				</li>
+			</ul>
+		</fieldset>
+	</form>
